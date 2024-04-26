7cf7703c2b8ae7cef537eee61fa2abfcc669303d
Kot4pa <beudydg835gd@gmail.com>
2024-04-26 15:11:37 +0500

    lab13

--- Lesson13/src/app/app.component.ts
+++ Lesson13/src/app/app.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { pizzas } from './data/pizzas';
+import { Pizza } from './models/pizza';
+import { Bavarian } from './models/bavarian-pizza';
+import { Margherita } from './models/margarita-pizza';
+import { Pepperoni } from './models/pepperoni-pizza';
+import { PizzaSizeService } from './services/PizzaSize.service';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Lesson13';
+export class AppComponent implements OnInit{
+  title = 'js-13';
+  data = pizzas;
+  selectedPizza : Pizza;
+  isPizzaSelected = false;
+  pizzaSize:string = "Маленькая";
+
+  constructor(private readonly pizzaSizeService : PizzaSizeService){}
+
+  ngOnInit(){
+    this.pizzaSizeService.isBigPizza$.subscribe((size) => this.setPizzaSize(size));
+  }
+  setPizzaSize(isBig:boolean){
+    if(isBig)
+      this.pizzaSize="Большая";
+    else
+      this.pizzaSize="Маленькая";
+    this.changePizza(this.selectedPizza);
+  }
+
+  changePizza(pizza:Pizza){
+    this.isPizzaSelected = true; 
+    if(pizza.stuff == "Пепперони"){
+      this.selectedPizza = new Pepperoni(this.pizzaSize);
+    }
+    else if(pizza.stuff == "Маргарита"){
+      this.selectedPizza = new Margherita(this.pizzaSize);
+    }
+    else {
+      this.selectedPizza = new Bavarian(this.pizzaSize);
+    }
+  }
 }
---
+++ Lesson13/src/app/data/toppings.ts
@@ -0,0 +1,7 @@
+import { Topping } from "../models/topping";
+
+export const toppings : Topping[] = [
+    new Topping("Сырный бортик", 50, 20, 100, 40, "./assets/Img/cheezeboard.jpg"),
+    new Topping("Сливочная моцарелла", 50, 20, 100, 50, "./assets/Img/mozzarella.jpg"),
+    new Topping("Чеддер и пармезан", 150, 50, 300, 100, "./assets/Img/parmezan.jpg")
+];
